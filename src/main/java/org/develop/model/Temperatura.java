f1dc65f2e115a4199d7d5282cb5f372ac54fd941
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-09-26 14:34:31 +0200

    Features: Use of Lombok on model

--- src/main/java/org/develop/model/Temperatura.java
+++ src/main/java/org/develop/model/Temperatura.java
@@ -2,12 +2,8 @@ package org.develop.model;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class Temperatura {
     private double degrees;
     private String hour;
-
-
 }
