266fa0d101d252d4ae4c79ca0d4ccaf8f355fe04
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-09-26 13:30:06 +0200

    Implementation of Lombon/Logger Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.projectlombok:lombok:1.18.30")
     // https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
     implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    implementation("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    // Logger
+    implementation("ch.qos.logback:logback-classic:1.4.11")
     // https://mvnrepository.com/artifact/com.opencsv/opencsv
     implementation("com.opencsv:opencsv:5.8")
         // Ibatis lo usaremos para leer los scripts SQL desde archivos
